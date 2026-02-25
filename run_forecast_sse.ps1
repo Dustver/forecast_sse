e6dddbea2db2c19b08b5c9f17bd331177de7d27c
Dustver <Dustver@users.noreply.github.com>
2026-02-25 17:59:48 +0300

    Change por to 50051 in run script

--- run_forecast_sse.ps1
+++ run_forecast_sse.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 # Some older `ServerSideExtension_pb2.py` generations require this with newer protobuf.
-$env:PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION = "python"
+#$env:PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION = "python"
 
-python .\ExtensionService_forecast.py --port 50053
+python .\ExtensionService_forecast.py --port 50051
 
